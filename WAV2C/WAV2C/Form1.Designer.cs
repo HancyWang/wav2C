f4cb6d064f8ae1c49bca23ab3d336708dcdd47ee
HancyWang <wanghouxun@126.com>
2019-09-09 15:21:50 +0800

    初版

---
+++ WAV2C/WAV2C/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace WAV2C
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button_import_file = new System.Windows.Forms.Button();
+            this.button_generate_C_file = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // button_import_file
+            // 
+            this.button_import_file.Location = new System.Drawing.Point(35, 34);
+            this.button_import_file.Name = "button_import_file";
+            this.button_import_file.Size = new System.Drawing.Size(107, 37);
+            this.button_import_file.TabIndex = 0;
+            this.button_import_file.Text = "导入WAV文件";
+            this.button_import_file.UseVisualStyleBackColor = true;
+            this.button_import_file.Click += new System.EventHandler(this.button_import_file_Click);
+            // 
+            // button_generate_C_file
+            // 
+            this.button_generate_C_file.Location = new System.Drawing.Point(35, 93);
+            this.button_generate_C_file.Name = "button_generate_C_file";
+            this.button_generate_C_file.Size = new System.Drawing.Size(107, 40);
+            this.button_generate_C_file.TabIndex = 1;
+            this.button_generate_C_file.Text = "生成C文件";
+            this.button_generate_C_file.UseVisualStyleBackColor = true;
+            this.button_generate_C_file.Click += new System.EventHandler(this.button_generate_C_file_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "WAV File(*.wav)|*.wav|All Files(*.*)|*.*";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 430);
+            this.Controls.Add(this.button_generate_C_file);
+            this.Controls.Add(this.button_import_file);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "WAV2C";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_import_file;
+        private System.Windows.Forms.Button button_generate_C_file;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ WAV2C/WAV2C/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WAV2C
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<Byte> m_list = new List<byte>();
+        long m_fileSize = 0;
+
+        private void button_import_file_Click(object sender, EventArgs e)
+        {
+            if (this.openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if (m_list.Count > 0)
+                {
+                    m_list.Clear();
+                }
+                //this.textBox_paraCfgFilePath.Text = this.openFileDialog1.FileName;
+                FileStream fs = new FileStream(this.openFileDialog1.FileName, FileMode.Open);
+                BinaryReader br = new BinaryReader(fs, Encoding.ASCII);
+
+                byte bt = new byte();
+
+                //System.Diagnostics.FileVersionInfo info=System.Diagnostics.FileVersionInfo.GetVersionInfo(this.openFileDialog1.FileName);
+
+                //获取文件信息
+                FileInfo fileInfo = new System.IO.FileInfo(this.openFileDialog1.FileName);
+
+                //根据文件长度(size)，将二进制内容放入链表中
+                m_fileSize = fileInfo.Length;
+                for (long i = 0; i < m_fileSize; i++)
+                {
+                    bt = br.ReadByte();
+                    m_list.Add(bt);
+                }
+
+                br.Close();
+                fs.Close();
+                MessageBox.Show("Load file successful!");
+            }
+
+        }
+
+        private void button_generate_C_file_Click(object sender, EventArgs e)
+        {
+            if (m_list.Count > 0)
+            {
+                if (this.folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    var path = this.folderBrowserDialog1.SelectedPath;
+
+                    //写配置文件1
+                    FileStream fs = new FileStream(path + @"\" + "WAV.c", FileMode.Create);
+                    StreamWriter sw = new StreamWriter(fs, Encoding.Default);
+
+                    string str = "const uint8_t DATA[]={";
+                    sw.WriteLine(str);
+
+
+                    for (int i = 0; i < m_fileSize / 16; i++)
+                    {
+                        string strTemp = "";
+                        for (int j = 0; j < 16; j++)
+                        {
+                            strTemp += "0x" + ConBverInt2Hex(m_list[i * 16 + j]) + ",";
+                        }
+                        //strTemp += "\n";
+                        sw.WriteLine(strTemp);
+                    }
+
+                    int rest = (int)(m_fileSize % 16);
+                    if (rest > 0)
+                    {
+                        string strTemp = "";
+                        for (int i = 0; i < rest; i++)
+                        {
+                            strTemp += "0x" + ConBverInt2Hex(m_list[((int)m_fileSize / 16) * 16 + i]) + ",";
+                        }
+                        //strTemp += "\n";
+                        sw.WriteLine(strTemp);
+                    }
+
+           
+                    sw.WriteLine("};");
+
+                    sw.Close();
+                    fs.Close();
+                    MessageBox.Show("WAV.c generate successful!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please load file first!");
+            }
+        }
+
+        string ConBverInt2Hex(byte bt)
+        {
+            string tmp = null;
+            Int32 a = Convert.ToInt32(bt) / 16;
+            Int32 b = Convert.ToInt32(bt) % 16;
+            //tmp += Convert.ToString(a);
+            if (a == 10)
+            {
+                tmp += "A";
+            }
+            else if (a == 11)
+            {
+                tmp += "B";
+            }
+            else if (a == 12)
+            {
+                tmp += "C";
+            }
+            else if (a == 13)
+            {
+                tmp += "D";
+            }
+            else if (a == 14)
+            {
+                tmp += "E";
+            }
+            else if (a == 15)
+            {
+                tmp += "F";
+            }
+            else
+            {
+                tmp += Convert.ToString(a);
+            }
+
+            if (b == 10)
+            {
+                tmp += "A";
+            }
+            else if (b == 11)
+            {
+                tmp += "B";
+            }
+            else if (b == 12)
+            {
+                tmp += "C";
+            }
+            else if (b == 13)
+            {
+                tmp += "D";
+            }
+            else if (b == 14)
+            {
+                tmp += "E";
+            }
+            else if (b == 15)
+            {
+                tmp += "F";
+            }
+            else
+            {
+                tmp += Convert.ToString(b);
+            }
+            return tmp;
+        }
+    }
+}
